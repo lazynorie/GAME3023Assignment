14663faad3e193542b903cd2c4ba5ce4adbec146
lazynorie <chengjy1991@gmail.com>
2021-12-01 17:25:53 -0500

    Added random enconter

--- Assets/_Scripts/PlayerBehaviour.cs
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 public class PlayerBehaviour : MonoBehaviour
 {
@@ -9,6 +12,13 @@ public class PlayerBehaviour : MonoBehaviour
     private Rigidbody2D rigidbody;
 
     private Animator animator;
+    
+    //Random Encounter
+    public LayerMask grassLayer;
+    [SerializeField]
+    private bool isMoving;
+    private float nextActionTime = 0.0f;
+    public float period = 0.1f;
     
     // Start is called before the first frame update
     void Start()
@@ -20,14 +30,68 @@ public class PlayerBehaviour : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        Move();
+    }
+
+    void Update()
+    {
+        if (isMoving)
+        {
+            if (Time.time > nextActionTime)
+            {
+                nextActionTime += period;
+                CheckEncounter();
+            }
+        }
+    }
+
+    private void CheckEncounter()
+    {
+        if (Physics2D.OverlapCircle(transform.position,0.001f,grassLayer) != null)
+        {
+            if (Random.Range(1, 101) <= 10)
+            {
+                Debug.Log("You've enter a randome encounter!");
+                
+                //Enter Random Encounter
+                //SceneManager.LoadScene("Encounter");
+            }
+        }
+        
+        
+    }
+
+    private void Move()
+    {
        
         float inputX = Input.GetAxisRaw("Horizontal");
         float inputY = Input.GetAxisRaw("Vertical");
         rigidbody.velocity = new Vector2(inputX * moveSpeed, inputY * moveSpeed);
-        
+
         animator.SetFloat("Horizontal", rigidbody.velocity.x);
         animator.SetFloat("Vertical", rigidbody.velocity.y);
-        animator.SetFloat("Speed",rigidbody.velocity.sqrMagnitude);
-
+        animator.SetFloat("Speed", rigidbody.velocity.sqrMagnitude);
+        if (rigidbody.velocity.sqrMagnitude > 0)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+       
+       
     }
+
+    
+    /*private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("LongGrass"))
+        {
+           
+            Debug.Log("hello");
+        }
+    }*/
+    
+
 }
