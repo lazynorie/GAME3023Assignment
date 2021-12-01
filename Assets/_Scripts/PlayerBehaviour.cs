fe2d5928cbe2559aa0631c33e376d0b30713a54e
lazynorie <chengjy1991@gmail.com>
2021-12-01 10:51:25 -0500

    Added player
    -Added animation
    -Added movement
    -Added camera

--- Assets/_Scripts/PlayerBehaviour.cs
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 1.0f;
+
+    private Rigidbody2D rigidbody;
+
+    private Animator animator;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+       
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+        rigidbody.velocity = new Vector2(inputX * moveSpeed, inputY * moveSpeed);
         
+        animator.SetFloat("Horizontal", rigidbody.velocity.x);
+        animator.SetFloat("Vertical", rigidbody.velocity.y);
+        animator.SetFloat("Speed",rigidbody.velocity.sqrMagnitude);
+
     }
 }
